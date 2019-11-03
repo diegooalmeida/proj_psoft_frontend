a46f34ef493710dc56a69c7ea4870c2c19cc9f36
Iago Tito <iago.oliveira@ccc.ufcg.edu.br>
2019-11-03 15:58:50 -0300

    Home page and sign up page (v0.2)

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 const API = "http://localhost:8080/v1/api";
-let users = [];
-
+let $viewer = document.querySelector("#viewer");
 let $message = document.querySelector("#message");
-let $button = document.querySelector("#button");
-$button.addEventListener("click", save);
-let $delete_button = document.querySelector("#delete_button");
 
-//todo: o delete e apenas para testar o app, retirar depois
-$delete_button.addEventListener("click", del);
-function del () {
-    fetch(API + "/users", {
-        method:"DELETE",
-        headers: {'Content-Type': 'application/json'}
-    })
-    .then(r => {
-        users = [];
-        fetch_users(users);
-    });
+function init () {
+    let $template = document.querySelector("#view_0");
+    $viewer.innerHTML = $template.innerHTML;
+
+    let $sign_up_button = document.querySelector("#sign_up_button");
+    $sign_up_button.addEventListener("click", sing_up_view);
+
+    //let $sign_in_button = document.querySelector("#sign_in_button");
+    //$sign_in_button.addEventListener("click", sing_in_view());
 }
 
-function create_user (email, fname, lname, password, credit_card) {
-    let user = {};
-    user.email = email;
-    user.fname = fname;
-    user.lname = lname;
-    user.password = password;
-    user.credit_card = credit_card;
-    return user;
+function sing_up_view () {
+    let $template = document.querySelector("#view_1");
+    $viewer.innerHTML = $template.innerHTML;
+
+    let $sign_up = document.querySelector("#sign_up");
+    $sign_up.addEventListener("click", sign_up);
+    let $cancel = document.querySelector("#cancel");
+    $cancel.addEventListener("click", cancel);
 }
 
-function save () {
+function cancel () {
+    $message.innerText = "";
+    init();
+}
+
+function sign_up () {
     /* >>> */ console.log("Salvando usuário");
 
     let email = document.querySelector("#email").value;
@@ -59,32 +58,28 @@ function save () {
             $message.innerText = "";
         }, 2000);
         console.log("Usuário cadastrado");
-        users.push(d);
-        fetch_users(users);
+        init();
     }
     });
 }
 
-function fetch_users (data) {
-    users = data;
-    console.log("Carregando usuários");
-    let $users = document.querySelector("#users_div");
-    $users.innerHTML = "";
-    users.forEach((e, i) => {
-        let $p = document.createElement("p");
-        $users.appendChild($p);
-        $p.innerText = "Nome: " + users[i].fname + " " + users[i].lname + "\nEmail: " + users[i].email;
-    });
-    console.log("Fim do carregamento de usuários");
+function create_user (email, fname, lname, password, credit_card) {
+    let user = {};
+    user.email = email;
+    user.fname = fname;
+    user.lname = lname;
+    user.password = password;
+    user.credit_card = credit_card;
+    return user;
 }
-
-function init() {
-    console.log("Iniciando página");
-    fetch(API + "/users")
-    .then(r => r.json())
-    .then(d => fetch_users(d));
-    console.log("Fim da inicialização");
-}
-
 init();
 
+//todo: o delete e apenas para testar o app, retirar depois
+let $delete_button = document.querySelector("#delete_button");
+$delete_button.addEventListener("click", del);
+function del () {
+    fetch(API + "/users", {
+        method:"DELETE",
+        headers: {'Content-Type': 'application/json'}
+    })
+}
