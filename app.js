68a3a09126a446e4ec1a11841d27968e60fa8865
Iago Tito <iago.oliveira@ccc.ufcg.edu.br>
2019-11-20 11:25:00 -0300

    Beginning of case 2: campaigns creation. TODOs pending in app.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const API = "http://localhost:8080/api";
-let $viewer = document.querySelector("#viewer");
+let $top = document.querySelector("#top");
+let $body = document.querySelector("#body");
 let $message_div = document.querySelector("#message_div");
 let token = undefined;
+
+/*
+    TODOS:
+    - falta fazer um jeito de mudar a url padrão caso ela já existe (caso exista uma campanha
+      com o memso nome).
+      Para isso é preciso recuperar no backend se o nome já existe, e se sim, recuperar o id numérico
+      da nova camapnha e colocar ele no fim da url. Isso poderia ser feito no backend, mas a especificação
+      diz que não. Melhor perguntar a Dalton, já que é uma pequena alteração;
+    - Fazer a listagem de campanhas na página inicial;
+    - Fazer o link para cada campanha;
+    - Após criar uma camapnha, redirecionar para a sua página.
+*/ 
 
 init();
 
@@ -15,26 +28,121 @@ function create_user (email, fname, lname, password, credit_card) {
     return user;
 }
 
+function create_campaign_object (name, description, deadline, goal) {
+    let campaign = {};
+    campaign.name = name;
+    campaign.url = ((name) => {
+        let url = name.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g," ").replace(/\s{2,}/g," ").trim().replace(/ /g,"-");
+        return url;
+    }) (name);
+    campaign.description = description;
+    campaign.deadline = deadline;
+    campaign.goal = goal;
+    return campaign;
+}
+
 function init () {
+    // Top of the site
+    if (is_logged()) load_logged_view();
+    else load_not_logged_view(); 
+
+    // Body of the site
+    load_home_view();
+}
+
+function load_home_view () {
     let $template = document.querySelector("#home_view");
-    $viewer.innerHTML = $template.innerHTML;
+    $body.innerHTML = $template.innerHTML;
 
-    let $sign_up_button = document.querySelector("#sign_up_button");
-    $sign_up_button.addEventListener("click", sign_up_view);
+    let $create_campaign = document.querySelector("#create_campaign");
+    $create_campaign.addEventListener("click", load_create_campaign_view);
+}
 
-    let $sign_in_button = document.querySelector("#sign_in_button");
-    $sign_in_button.addEventListener("click", sign_in_view);
+function load_create_campaign_view () {
+    console.log("load_create_campaign_view ()");
+    if (!is_logged()) {
+        console.log("User not logged.");
+
+        $body.innerHTML = "";
+
+        let $p = document.createElement("P");
+        $p.innerText = "Você precisa estar logado para criar uma campanha.";
+
+        let $back_button = document.createElement("BUTTON");
+        $back_button.innerText = "Voltar";
+        $back_button.addEventListener("click", load_home_view);
+
+        $body.appendChild($p);
+        body.appendChild($back_button);
+    } else {
+
+        console.log("Logged user, loading view to insert campaign data.");
+        let $template = document.querySelector("#create_campaign_view");
+        $body.innerHTML = $template.innerHTML;
+
+        let $cancel = document.querySelector("#cancel");
+        $cancel.addEventListener("click", cancel);
+
+        let $create_campaign_button = document.querySelector("#create_campaign_button")
+        $create_campaign_button.addEventListener("click", create_campaign);
+    }
+}
+
+function create_campaign () {
+    let name = document.querySelector("#name").value;
+    let description = document.querySelector("#description").value;
+    let deadline = document.querySelector("#deadline").value;
+    let goal = document.querySelector("#goal").value.replace(',','.').replace(' ','');
+
+    if (deadline.length !== 10 || is_in_the_past(deadline)) {
+        $message_div.innerText = "Data inválida. Escreva uma data no formato dd/mm/yyyy e que ainda não tenha se passado.";
+    } else {
+        $message_div.innerText = "";
+        let campaign = create_campaign_object(name, description, deadline, goal);
+        fetch (API + "/campaigns/create", {
+            method:"POST",
+            headers: {"Content-Type":"application/json",
+                      "Authorization":"Bearer " + token},
+            body: JSON.stringify(campaign)
+        })
+        .then(r => r.json())
+        .then(d => {
+            console.log("Campanha criada com sucesso.");
+            console.log(d);
+        });
+    }
+}
+
+function is_in_the_past (date) {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (parseInt(date.substring(6)) > parseInt(yyyy))
+        return false;
+    else if (parseInt(date.substring(3,5)) > parseInt(mm))
+        return false;
+    else if (parseInt(date.substring(0,2)) > parseInt(dd))
+        return false;
+    else 
+        return true;
+}
+
+function is_logged () {
+    if (token === undefined) return false;
+    return true;
 }
 
 function cancel () {
+    console.log("Canceled action");
     $message_div.innerHTML = "";
-    token = undefined;
     init();
 }
 
-function sign_up_view () {
+function load_sign_up_view () {
     let $template = document.querySelector("#sign_up_view");
-    $viewer.innerHTML = $template.innerHTML;
+    $top.innerHTML = $template.innerHTML;
 
     let $sign_up = document.querySelector("#sign_up");
     $sign_up.addEventListener("click", sign_up);
@@ -43,7 +151,7 @@ function sign_up_view () {
 }
 
 function sign_up () {
-    /* >>> */ console.log("Salvando usuário");
+    /* >>> */ console.log("Registering user");
 
     let email = document.querySelector("#email").value;
     let fname = document.querySelector("#fname").value;
@@ -61,8 +169,8 @@ function sign_up () {
     .then(r => {
         if (r.ok) return r.json();
         else {
-            console.log("Email já cadastrado");
-            $message_div.innerText = "Email já cadastrado, tento novamente";
+            console.log("Email already exists");
+            $message_div.innerText = "Email já cadastrado, tente novamente";
             $message_div.append(document.createElement("hr"));
         }
     })
@@ -73,15 +181,15 @@ function sign_up () {
         setTimeout(_ => {
             $message_div.innerHTML = "";
         }, 2000);
-        console.log("Usuário cadastrado");
+        console.log("User registered");
         init();
     }
     });
 }
 
-function sign_in_view () {
+function load_sign_in_view () {
     let $template = document.querySelector("#sign_in_view");
-    $viewer.innerHTML = $template.innerHTML;
+    $top.innerHTML = $template.innerHTML;
 
     let $sign_in = document.querySelector("#sign_in");
     $sign_in.addEventListener("click", sign_in);
@@ -122,15 +230,26 @@ function sign_in () {
                 $message_div.innerHTML = "";
             }, 2000);
             console.log("Login realizado");
-            logged_user_view();
+            load_logged_view();
         }
         
     });
 }
 
-function logged_user_view () {
-    let $template = document.querySelector("#logged_user_view");
-    $viewer.innerHTML = $template.innerHTML;
+function load_not_logged_view () {
+    let $template = document.querySelector("#not_logged_view");
+    $top.innerHTML = $template.innerHTML;
+
+    let $sign_up_button = document.querySelector("#sign_up_button");
+    $sign_up_button.addEventListener("click", load_sign_up_view);
+
+    let $sign_in_button = document.querySelector("#sign_in_button");
+    $sign_in_button.addEventListener("click", load_sign_in_view);
+}
+
+function load_logged_view () {
+    let $template = document.querySelector("#logged_view");
+    $top.innerHTML = $template.innerHTML;
 
     let $button = document.querySelector("#logout_button");
     $button.addEventListener("click", cancel);
@@ -146,19 +265,8 @@ function logged_user_view () {
     })
     .then(r => r.json())
     .then(d => {
-        console.log("d");
         $user = d;
         $p.innerText = "Olá " + $user.fname + " " + $user.lname;
     });
 
-}
-
-//todo: o delete e apenas para testar o app, retirar depois
-let $delete_button = document.querySelector("#delete_button");
-$delete_button.addEventListener("click", del);
-function del () {
-    fetch(API + "/users", {
-        method:"DELETE",
-        headers: {'Content-Type': 'application/json'}
-    })
 }
